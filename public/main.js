4cd48495919ffbf11dc48c8b3f5bc8085a64f27b
thiago <thiagoferolla2@gmail.com>
2017-03-19 22:38:37 -0300

    navbar otimization

--- public/main.js
+++ public/main.js
@@ -47,7 +47,13 @@ app.controller('tasks', function($scope, $http, $interval){
 
 $(document).ready(function(){
   $('.modal').modal();
-  $(".button-collapse").sideNav();
+    $('.button-collapse').sideNav({
+      menuWidth: 300,
+      edge: 'right',
+      closeOnClick: true,
+      draggable: true 
+    }
+  );
 });
 
 
