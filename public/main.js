9e0857bacd1eeec2cbc70a40a62f77c2ff4d7021
thiago <thiagoferolla2@gmail.com>
2017-03-20 22:07:18 -0300

    -n

--- public/main.js
+++ public/main.js
@@ -50,12 +50,7 @@ app.controller('tasks', function($scope, $http, $interval){
 
 $(document).ready(function(){
   $('.modal').modal();
-  $('.button-collapse').sideNav({
-    menuWidth:500,
-    edge:"right",
-    closeOnClick: true,
-    draggable:true
-  });
+  $('.button-collapse').sideNav();
 });
 
 
